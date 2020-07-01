1b5596fdbb8599982a00248d3b0ce84c082fa49d
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-07-01 13:13:37 +0300

    Fixed response data mistake

--- number-division/app.py
+++ number-division/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 
 from database import DB
 
@@ -11,19 +12,26 @@ app = Flask(__name__)
 def index():
     number = None
     if request.method == 'POST':
-        try:
-            a = float(request.form["numA"])
-            b = float(request.form["numB"])
-            if b == 0:
-                raise ZeroDivisionError
-        except ValueError:
-            return render_template('index.html', error="Must imput numbers")
-        except ZeroDivisionError:
-            return render_template('index.html', error="Number B mustn't be zero")
-        with DB() as db:
-            db.execute("INSERT INTO numbers (numA, numB, data) VALUES (%s, %s, %s)",
-                        (a, b, request.form["data"]))
-            db.execute("SELECT id, numA, numB FROM numbers ORDER BY id DESC")
-            id, numA, numB = db.fetchone()
-            number = numA/numB + id
-    return render_template('index.html', number=number)
+        if request.is_json:
+            json = request.get_json()
+            try:
+                a = float(json["numA"])
+                b = float(json["numB"])
+                if b == 0:
+                    raise ZeroDivisionError
+            except ValueError:
+                return "You must enter numbers", 400
+            except ZeroDivisionError:
+                return "Number B mustn't be zero", 400
+            with DB() as db:
+                db.execute("INSERT INTO numbers (numA, numB, data) VALUES (%s, %s, %s)",
+                            (a, b, json["data"]))
+                db.execute("SELECT id, numA, numB FROM numbers ORDER BY id DESC")
+                id, numA, numB = db.fetchone()
+                number = numA/numB
+            return jsonify({"result": number, "row":id}), 201
+        
+            
+        return "nice", 201
+    elif request.method == 'GET':
+        return render_template('index.html', number=number), 200
